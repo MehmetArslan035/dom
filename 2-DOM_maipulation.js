809f9ff5ca2c00443143e6809b5b51e0834e5360
mehmet <mehmet00arslan@gmail.com>
2024-07-27 13:37:50 +0300

    add

--- 2-DOM_maipulation.js
+++ 2-DOM_maipulation.js
@@ -1,1 +1,19 @@
 /* -------- DOM MANIPULATION -------- */
+
+
+// 1-Eleman Ekleme
+const movieUl=document.getElementById("movie-ul")
+const yeniEleman=document.createElement("li")
+yeniEleman.textContent="Blacklist"
+yeniEleman.className="movie-item"
+
+movieUl.appendChild(yeniEleman)
+
+
+// 2- Yeni Eleman eklemek için daha kolay yöntem
+movieUl.innerHTML += `<li id="avatar" class="movie-item avatar">God Father</li>`
+
+
+// 2- Element Silme
+
+movieUl.removeChild(matrix)
